27db947577611f749435616b6742453dbd866254
josi-lima <teacher.josi@yahoo.com>
2023-08-06 00:00:50 -0300

    :books: feat: add data types examples

--- day01/HelloWorld.py
+++ day01/HelloWorld.py
@@ -1,1 +1,33 @@
 print('Hello World!');
+
+# ===========================================================
+
+# Basic data types 
+
+# negative integer
+temperature = -15; 
+
+# positive integer 
+day = 6; 
+
+# float
+weight = 59.2; 
+
+# string
+favePizzaTopping = "broccoli with dried tomatoes";
+
+# string with backslash
+faveRestaurant = 'Josie\'s Vegan Delicious "Burgers"';
+
+# ===========================================================
+
+# concatenation with a comma
+print("The current temperature feels like:", temperature); 
+
+# concatenation with f- strings
+print(f"Today is August, the {day}th.")
+print(f"Your total weight goal is: {weight - 5}.");
+print(f"My favorite pizza topping is {favePizzaTopping}.");
+print(f"My favorite restaurant of all time is {faveRestaurant}.");
+
+
