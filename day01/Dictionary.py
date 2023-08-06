2a6ee70abd6bfb33b0ac7c8c332ca1a0779f96ce
josi-lima <teacher.josi@yahoo.com>
2023-08-06 12:24:39 -0300

    :books: feat: add dictionary

---
+++ day01/Dictionary.py
@@ -0,0 +1,24 @@
+# Dictionaries
+
+# They are lists of Key: Value pairs, and can contain heterogeneous keys as well as values.
+
+room_nums = {'Tom': 403, 'Jerry': 596}
+room_nums['Tom'] = 753  # set the value associated with the ’Tom’ key to 753
+
+print(room_nums) 
+# {'Tom': 753, 'Jerry': 596}
+
+print(room_nums['Jerry'])  
+# 596
+
+room_nums['Isaac'] = 345  # add a new key ’Isaac’ with the associated value
+
+print(room_nums.keys()) 
+# dict_keys(['Tom', 'Jerry', 'Isaac'])
+
+print ('Isaac' in room_nums)  # test if ’issac’ is in the dictionary
+# True
+
+# different types of values in a single list
+hotel_features = {1: "Taj", "city": "Woodland", "distance": 3.14}
+print(hotel_features)
