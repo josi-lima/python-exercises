377c3660cb4ad9fa063f122d0d46423503330d99
josi-lima <teacher.josi@yahoo.com>
2023-08-08 15:44:14 -0300

    :bulb: docs: add comments to the class

--- day03/Dog.py
+++ day03/Dog.py
@@ -1,11 +1,12 @@
 class Dog:
+  
   definition = "a domesticated carnivorous mammal that typically has a long snout, an acute sense of smell, nonretractable claws, and a barking, howling, or whining voice."
   
-  # initialization function: whenever a new object is created from a class, this function is called. It's similar to a constructor in Java
+  # initialization function: whenever a new object is created from a class, this function is called. It's similar to a constructor in Java.
   def __init__(self):
     print("I'm alive!")
 
-# class variable or an instance
+# an object or an instance of a class
 Dog()
   
 # access a variable from a class by using the dot operator
