9bbde21e1db89831dd5030b5f7033131edd4f826
josi-lima <teacher.josi@yahoo.com>
2023-08-11 03:07:13 -0300

    :books: feat: update dict examples

--- day01/Dictionary.py
+++ day01/Dictionary.py
@@ -4,7 +4,8 @@
 
 room_nums = {
   'Tom': 403, 
-  'Jerry': 596}
+  'Jerry': 596
+  }
 
 # ---------------------------
 
@@ -48,7 +49,8 @@ print(room_nums)
 hotel_features = {
   1: 'Taj', 
   'city': 'Woodland', 
-  'distance': 3.14}
+  'distance': 3.14
+  }
 
 print(hotel_features)
 # {1: 'Taj', 'city': 'Woodland', 'distance': 3.14}
@@ -58,6 +60,17 @@ print(hotel_features)
 # Iterating a dictionary
 
 # loop over the keys in the dictionary, rather than the keys and values
+
+counts = { 'chuck': 10, 'annie': 42, 'jane': 100 }
+
+for key in counts:
+  if counts[key] > 10:
+    print(key, counts[key])
+        
+# annie 42
+# jane 100
+        
+# ---------------------------
 
 states_Brazil = {
   'SP': 'Sao Paulo', 
@@ -79,13 +92,28 @@ for key, value in states_Brazil.items():   # 'key' and 'value' are just variable
   print(f"{key} is the acronym for {value}.")
   
 # Example --> SP is the acronym for Sao Paulo.
+
+# ---------------------------
+
+shapes = dict()
+
+shapes['square'] = 1
+shapes['circle'] = 5
+shapes['triangle'] = 9
+
+for (k, i) in shapes.items():
+    print(k, i)
+
+# square 1
+# circle 5
+# triangle 9
   
 # ===============================================
 
 # Order in a dictionary
 
-# Lists --- index their entries based on the position in the list. 
-# Dictionaries --- on the other hand, are like bags: no order!
+# Lists index their entries based on the position in the list. 
+# Dictionaries, on the other hand, are like bags: no order!
 
 dict = {"Fri": 20, "Thu": 6, "Sat": 1}
 
