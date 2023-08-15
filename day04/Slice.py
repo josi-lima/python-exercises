3d165db8db0fb92d1ed7a023c6ea8fc57184185f
josi-lima <teacher.josi@yahoo.com>
2023-08-15 16:07:48 -0300

    :bulb: docs: change comments on slice examples

--- day04/Slice.py
+++ day04/Slice.py
@@ -2,7 +2,7 @@
 
 pizza = ['rúcula com tomate seco', '4 queijos', 'milho', 'frango vegano']
 
-# index from 0 to 2. The last one is not included, and index 0 can be omitted
+# index from 0 up to 2. The last one is not included, and index 0 can be omitted
 print(pizza[:2])
 # ['rúcula com tomate seco', '4 queijos']
 
@@ -12,7 +12,7 @@ print(pizza[:2])
 
 quoteA = "So you failed." 
 
-# index from 0 onwards, and index from 3 onwards
+# index from 0 onwards, and from index 3 onwards
 print(quoteA[:])
 print(quoteA[3:])
 # So you failed.
@@ -22,7 +22,7 @@ print(quoteA[3:])
 
 quoteB = "You wanna be really great? Then have the courage to fail big and stick around." 
 
-# index from 0 to 26. The last one is not included, and index 0 can be omitted
+# index from 0 up to 26. The last one is not included, and index 0 can be omitted
 print(quoteB[:26])
 # You wanna be really great?
 
@@ -30,7 +30,7 @@ print(quoteB[:26])
 
 quoteC = "Make them wonder why you're still smiling."
 
-# index from 0 to 16, the last one is not included
+# index from 0 up to 16, the last one is not included
 print(quoteC[0:16]) 
 # Make them wonder
 
