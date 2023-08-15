b7a4da6344497d8f9a26c64598bdde5f3c11ebd4
josi-lima <teacher.josi@yahoo.com>
2023-08-15 16:02:55 -0300

    :books: feat: add slice examples

---
+++ day04/Slice.py
@@ -0,0 +1,41 @@
+# slicing elements from a list
+
+pizza = ['rúcula com tomate seco', '4 queijos', 'milho', 'frango vegano']
+
+# index from 0 to 2. The last one is not included, and index 0 can be omitted
+print(pizza[:2])
+# ['rúcula com tomate seco', '4 queijos']
+
+# ===========================================================
+
+# slicing characters from a string
+
+quoteA = "So you failed." 
+
+# index from 0 onwards, and index from 3 onwards
+print(quoteA[:])
+print(quoteA[3:])
+# So you failed.
+# you failed.
+
+# ---------------------------
+
+quoteB = "You wanna be really great? Then have the courage to fail big and stick around." 
+
+# index from 0 to 26. The last one is not included, and index 0 can be omitted
+print(quoteB[:26])
+# You wanna be really great?
+
+# ---------------------------
+
+quoteC = "Make them wonder why you're still smiling."
+
+# index from 0 to 16, the last one is not included
+print(quoteC[0:16]) 
+# Make them wonder
+
+# ===========================================================
+
+
+
+
